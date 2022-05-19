02eeb88b338e4a63324ee6b723f8e0aa09b1bf9f
kartikp36 <kartikp2002@gmail.com>
2022-05-19 19:12:55 +0530

    fix: word-break and date

--- src/DisplayTodos.jsx
+++ src/DisplayTodos.jsx
@@ -11,26 +11,24 @@ export const DisplayTodos = ({ level, todos }) => {
           key={todo.check}
           type="checkbox"
           id={todo.key}
-          className={todo.text}
+          className={todo.key}
           value={todo.text}
-          label={todo.text}
+          label={todo.key}
           defaultChecked={todo.check ? true : false}
         />
-        <label className="label" htmlFor={todo.key}>
-          {`${todo.text}` +
-            "    " +
-            " (" +
-            new Date(todo.timestamp).toLocaleString() +
-            ")"}
+        <label
+          className="label"
+          htmlFor={todo.key}>
+          {`${todo.text}    `}
         </label>
+        <br />
+        <small className="label-date">
+          {new Date(todo.timestamp).toLocaleString()}
+        </small>
       </li>
     );
   });
 
-  return (
-    <div>
-      {listItem}
-    </div>
-  );
+  return <div>{listItem}</div>;
 };
 export default DisplayTodos;
